062b5eaf7ace8ee80cf14864a1886f38107ff0be
dpbhalani <dharmeshbhalani1712@gmail.com>
2023-03-09 18:57:53 +0530

    create a first ExpenseItem component and implement it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import ExpenseIteam from "./component/ExpenseItem";
+
 function App () {
   return(
     <div>
       <h1>Let's get started...!</h1>
+      <ExpenseIteam></ExpenseIteam>
     </div>
   );
 }
