78d77bc1752a62f185e7d704f2acb3cb763eb9e1
dpbhalani <dharmeshbhalani1712@gmail.com>
2023-03-09 19:24:21 +0530

    added dynamic code for date and title

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css'
 
 function ExpenseItem() {
+    const expenseDate = new Date(2023, 3 ,10);
+    const expenseTitle = "car insurance";
+    const expensePrice = 153.25; 
+
   return (
     <div className='expense-item'>
-      <div>Date:- march 9 2023</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className='expense-item__description'>
-        <h3>car insurance</h3>
-        <div className='expense-item__price'>$153.25</div>
+        <h3>{expenseTitle}</h3>
+        <div className='expense-item__price'>${expensePrice}</div>
       </div>
     </div>
   );
