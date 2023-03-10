d13f07fbf4f3a3362bb94d851019f73c208f483a
dpbhalani <dharmeshbhalani1712@gmail.com>
2023-03-10 13:38:28 +0530

    solve css problem

--- src/component/Expenses/ExpenseItem.js
+++ src/component/Expenses/ExpenseItem.js
@@ -8,7 +8,7 @@ const ExpenseItem = (props)=>{
     <Card className="expense-item">
         <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h3>{props.title}</h3>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
     </Card>
