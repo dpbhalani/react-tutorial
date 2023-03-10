de61a2cf8c6cef34e851f579e1e75d84eec7ff90
dpbhalani <dharmeshbhalani1712@gmail.com>
2023-03-10 14:26:08 +0530

    props.title is remove and add a title

--- src/component/Expenses/ExpenseItem.js
+++ src/component/Expenses/ExpenseItem.js
@@ -3,15 +3,18 @@ import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props)=>{
+   let title = props.title;
+
   const clickHandler = () => {
-    console.log("clicked!!!!");
+    title="Updated..";
+    console.log(title);
   }
 
   return (
     <Card className="expense-item">
         <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change title</button>
