fb206df78d6958138627d56afd2a075a9f3acb78
dpbhalani <dharmeshbhalani1712@gmail.com>
2023-03-10 19:45:52 +0530

    create a ExpenseList file and filterinfg the data

--- src/component/Expenses/Expenses.js
+++ src/component/Expenses/Expenses.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
+//import ExpenseItem from "./ExpenseItem";
+import ExpensesList from "./ExpensesList";
 import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
@@ -15,19 +16,6 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p>No expenses found</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
-
   return (
     <Card className="expenses">
       <ExpensesFilter
@@ -35,7 +23,7 @@ const Expenses = (props) => {
         onChangeFilter={filterChangeHandler}
       />
 
-      {expensesContent}
+      <ExpensesList items={filteredExpenses}/>
     </Card>
   );
 };
