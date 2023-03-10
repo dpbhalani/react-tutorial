b86dcbd992e112c2d2736dfcccd54b133a111561
dpbhalani <dharmeshbhalani1712@gmail.com>
2023-03-10 16:19:20 +0530

    add multiple state handler

--- src/component/NewExpense/ExpenseForm.js
+++ src/component/NewExpense/ExpenseForm.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+    const [enteredTitle , setEnteredTitle] = useState('');
+    const [enteredAmount , setEnteredAmount] = useState('');
+    const [enteredDate , setEnteredDate] = useState('');
+    
     const titleChangeHandler = (event) => {
-        console.log(event.target.value);
-    }
+        setEnteredTitle(event.target.value);
+    };
+
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value);
+    };
+
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value);
+    };
+
     return(
         <form>
         <div className="new-expense__controls">
