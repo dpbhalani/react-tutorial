e0793c24cce1a6bfd88d6ef3575c3c47f53c746f
dpbhalani <dharmeshbhalani1712@gmail.com>
2023-03-10 17:42:11 +0530

    add child to parent communication flow

--- src/component/NewExpense/NewExpense.js
+++ src/component/NewExpense/NewExpense.js
@@ -3,10 +3,19 @@ import './newExpense.css';
 import ExpenseForm from "./ExpenseForm";
 
 
-const newExpense = () => {
+const newExpense = (props) => {
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id:Math.random().toString()
+        }
+        //console.log(expenseData);
+        props.onAddExpense(expenseData);
+    };
+
  return(
     <div className="new-expense">
-        <ExpenseForm />
+        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
  );
 };
